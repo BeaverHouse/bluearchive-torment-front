c4684a9e14dc29c42fc7fa6036fa7a5fe91084c6
Austin Lee <haulrest@gmail.com>
2025-07-28 15:48:05 +0900

    Remove API dependency in MainPage

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,52 +1,22 @@
 import useBAStore from "../useV3Store";
-import { useQuery } from "@tanstack/react-query";
 import RaidSearch from "./molecules/RaidSearch";
 import RaidSummary from "./molecules/RaidSummary";
 import InfoFAB from "./atoms/InfoFAB";
 import NormalAnnounce from "./atoms/NormalAnnounce";
+import studentsData from "../data/students.json";
+import raidsData from "../data/raids.json";
 import Typography from "antd/es/typography";
 import Tabs, { TabsProps } from "antd/es/tabs";
 import Select from "antd/es/select";
 import Button from "antd/es/button";
-import Spin from "antd/es/spin";
 
 const { Title } = Typography;
 
 function MainPage() {
   const { V3Season, setV3Season } = useBAStore();
 
-  const studentsQuery = useQuery({
-    queryKey: ["getStudents"],
-    queryFn: async () => {
-      const res = await fetch(`${import.meta.env.VITE_API_URL}/students`, {
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-        },
-      });
-      return res.json();
-    },
-    throwOnError: true,
-  });
-
-  const raidsQuery = useQuery({
-    queryKey: ["getRaids"],
-    queryFn: async () => {
-      const res = await fetch(`${import.meta.env.VITE_API_URL}/v2/raids`, {
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-        },
-      });
-      return res.json();
-    },
-    throwOnError: true,
-  });
-
-  if (studentsQuery.isLoading || raidsQuery.isLoading) {
-    return <Spin spinning={true} fullscreen />;
-  }
-
-  const studentsMap = studentsQuery.data as Record<string, string>;
-  const raidInfos = (raidsQuery.data as RaidInfo[]).map((raid) => ({
+  const studentsMap = studentsData as Record<string, string>;
+  const raidInfos = (raidsData as RaidInfo[]).map((raid) => ({
     value: raid.id,
     label: raid.description,
     topLevel: raid.top_level,
