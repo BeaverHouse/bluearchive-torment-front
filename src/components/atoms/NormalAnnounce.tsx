c4babe8d4ec3bd3b69a41319d7f6ab48935952ec
Austin Lee <haulrest@gmail.com>
2025-08-03 23:59:47 +0900

    Fix announcement

--- src/components/atoms/NormalAnnounce.tsx
+++ src/components/atoms/NormalAnnounce.tsx
@@ -5,21 +5,11 @@ function NormalAnnounce() {
     <Alert
       style={{ marginBottom: 10, width: "100%", textAlign: "left" }}
       showIcon
-      type="info"
-      message="서비스 업데이트 종료 안내"
+      type="warning"
+      message="임시 데이터 업데이트 (S79 실내 고즈)"
       description={
         <div>
-          이제 더 이상 업데이트가 되지 않을 예정입니다. 기존 데이터는 계속 조회
-          가능합니다.
-          <br />
-          자세한 내용은{" "}
-          <a
-            href="https://github.com/BeaverHouse/bluearchive-torment-front?tab=readme-ov-file#-end-of-service"
-            target="_blank"
-          >
-            링크
-          </a>
-          를 참고해 주세요.
+          아직 자동화 방법과 지속 가능성을 판단하는 중입니다.
         </div>
       }
     />
