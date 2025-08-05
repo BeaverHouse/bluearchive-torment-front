11325b8c6094546ee7a5f84acade77c2e5bd8e60
Austin Lee <haulrest@gmail.com>
2025-08-05 23:22:39 +0900

    Update announcement

--- src/components/atoms/NormalAnnounce.tsx
+++ src/components/atoms/NormalAnnounce.tsx
@@ -5,11 +5,17 @@ function NormalAnnounce() {
     <Alert
       style={{ marginBottom: 10, width: "100%", textAlign: "left" }}
       showIcon
-      type="warning"
-      message="임시 데이터 업데이트 (S79 실내 고즈)"
+      type="info"
+      message="임시 데이터 업데이트"
       description={
         <div>
-          아직 자동화 방법과 지속 가능성을 판단하는 중입니다.
+          데이터 획득 + 자동화 방법을 찾았고, 카이텐 대결전까지 최신 업데이트가 된 상태입니다.
+          <br />
+          다만, 재정비와 검토가 여전히 필요하다고 판단되어 약간의 시간을 가지려고 합니다.
+          <br />
+          데이터 소스에 대해서는 <a href="https://github.com/BeaverHouse/ba-torment-data-process/discussions/10" target="_blank">이곳</a>을 참고해 주세요.
+          <br />
+          필요하다면 따로 공지를 남기도록 하겠습니다. 감사합니다.
         </div>
       }
     />
