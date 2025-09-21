c037aee7f75c21e43a1ce5f4fa52b40fc1e69fd9
Austin Lee <haulrest@gmail.com>
2025-09-21 21:41:37 +0900

    Integrate config file
    나누어져 있어서 수정합니다.

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  distDir: 'out',
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
   assetPrefix: process.env.NODE_ENV === 'production' ? './' : '',
   basePath: process.env.NODE_ENV === 'production' ? '' : '',
+  async rewrites() {
+    return [
+      {
+        source: '/ba-analyzer/v1/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8085'}/ba-analyzer/v1/:path*`,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
