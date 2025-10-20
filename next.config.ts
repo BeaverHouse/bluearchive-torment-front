392b50b998d1c1f2ab8605f74707bac4a5815cd6
Austin Lee <haulrest@gmail.com>
2025-10-21 02:21:55 +0900

    fix: [DEV-39] Change to standalone, reduce load (#57)

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/ba-analyzer/v1/:path*',
-        destination: `${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8085'}/ba-analyzer/v1/:path*`,
-      },
-    ]
-  },
+  output: 'standalone',
 };
 
 export default nextConfig;
