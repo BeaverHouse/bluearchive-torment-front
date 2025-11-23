54c9d7337fa739f7b31b78305ecd3737fef90403
Austin Lee <haulrest@gmail.com>
2025-11-23 16:08:42 +0900

    Update announce

--- src/components/common/normal-announce.tsx
+++ src/components/common/normal-announce.tsx
@@ -5,12 +5,12 @@ function NormalAnnounce() {
   return (
     <Alert className="mb-3 w-full text-left">
       <Info className="h-4 w-4" />
-      <AlertTitle>25.11.09 Update</AlertTitle>
+      <AlertTitle>25.11.23 Update</AlertTitle>
       <AlertDescription className="mt-2">
         <div className="mb-2">
-          1. 예소드 정보를 추가했어요.
+          1. 예로니무스 대결전 정보를 추가했어요.
           <br />
-          2. 총력전 점수 계산기가 이제 4분 30초 옵션을 지원해요.
+          2. 이제 별명으로도 학생을 검색할 수 있어요. (ex. 제왓삐, 수즈사)
         </div>
       </AlertDescription>
     </Alert>
