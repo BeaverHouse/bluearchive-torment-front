bca3d5e967cb752b1a29755a1848d5c87f91bd64
Austin Lee <haulrest@gmail.com>
2025-12-21 03:05:02 +0900

    Update announce

--- src/components/common/normal-announce.tsx
+++ src/components/common/normal-announce.tsx
@@ -5,12 +5,12 @@ function NormalAnnounce() {
   return (
     <Alert className="mb-3 w-full text-left">
       <Info className="h-4 w-4" />
-      <AlertTitle>25.12.07 Update</AlertTitle>
+      <AlertTitle>25.12.21 Update</AlertTitle>
       <AlertDescription className="mt-2">
         <div className="mb-2">
-          1. 쿠로카게 총력전 정보를 추가했어요.
+          1. 카이텐 대결전 정보를 추가했어요.
           <br />
-          2. 요약 탭에서 볼 수 있는 정보가 늘어났어요.
+          2. 모바일과 요약 탭 UI를 약간 수정했어요.
         </div>
       </AlertDescription>
     </Alert>
